7d93996b00616f97f64870dcdeff1604f9d99273
Serhii Repinskyi <repinskyisv@gmail.com>
2025-09-18 22:24:46 +0300

    Add dashboard slots

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-// import MagicButton from '@/app/components/magic-button';
+import Header from '@/app/components/header';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      {/* <MagicButton /> */}
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
