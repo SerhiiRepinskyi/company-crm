3e856e28fa8d3fedb3255952ab68f4ccf74f7347
Serhii Repinskyi <repinskyisv@gmail.com>
2025-10-20 23:25:01 +0300

    Add using fetch

--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import { getSummaryStats } from '@/lib/api';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
 import StatCard, { StatCardType } from '@/app/components/stat-card';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface PageProps {}
 
-const labelByStat = {
+const labelByStat: Record<keyof SummaryStats, string> = {
   promotions: 'Total promotions',
   categories: 'Total categories',
   newCompanies: 'New companies',
@@ -15,10 +15,11 @@ const labelByStat = {
 
 export default async function Page({}: PageProps) {
   const data = await getSummaryStats();
+  // const data = await getSummaryStats({ next: { revalidate: 5 } });
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
         <Link href={`/dashboard/${key}`} key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
